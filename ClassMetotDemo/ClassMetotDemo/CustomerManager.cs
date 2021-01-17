ae55bef03c82c77c66929051aa03c523656579f6
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2021-01-17 17:11:50 +0200

    Update-Delete

--- ClassMetotDemo/ClassMetotDemo/CustomerManager.cs
+++ ClassMetotDemo/ClassMetotDemo/CustomerManager.cs
@@ -6,6 +6,9 @@ namespace ClassMetotDemo
 {
     class CustomerManager
     {
+        Customer customer3 = new Customer { Id = 3, Name = "Bedirhan", Surname = "Aygün", Adress = "Üsküdar" };
+        Customer customer2 = new Customer { Id = 2, Name = "Batuhan", Surname = "Aygün", Adress = "Üsküdar" };
+
         public void Add()
         {
             Customer customer = new Customer();
@@ -18,11 +21,11 @@ namespace ClassMetotDemo
         }
         public void Update()
         {
-            Console.WriteLine("Customer Updated!");
+            Console.WriteLine(customer2.Name + " : Customer Updated!");
         }
         public void Delete()
         {
-            Console.WriteLine("Customer Deleted!");
+            Console.WriteLine(customer3.Name+ " : Customer Deleted!");
         }
     }
 }

77a8abbaa309d3588a792f0fb5586e08f3e0abcf
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2021-01-17 16:50:33 +0200

    Customer-All-Commit

---
+++ ClassMetotDemo/ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added!");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated!");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("Customer Deleted!");
+        }
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customer = new CustomerManager();
+            customer.Add();
+            customer.Update();
+            customer.Delete();
+
+            Console.ReadLine();
+        }
+    }
+}
